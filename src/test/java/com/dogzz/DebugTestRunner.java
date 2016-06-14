45a8a4b5a3b54ee65065821c81937fb913bac4e0
dogzz <afon@ukr.net>
2016-06-14 16:51:29 +0300

    Disable Debug Test Runner

--- src/test/java/com/dogzz/DebugTestRunner.java
+++ src/test/java/com/dogzz/DebugTestRunner.java
@@ -8,14 +8,14 @@ package com.dogzz;
 import net.serenitybdd.jbehave.SerenityStories;
 import org.jbehave.core.steps.ParameterControls;
 
-public class DebugTestRunner extends SerenityStories {
-
-    public DebugTestRunner() {
-        findStoriesCalled("stories/WebServicesInteractions/ResourceService/*");
-//        findStoriesCalled("stories/WebServicesInteractions/HTTPService/PostRequests.story;" +
-//                "stories/WebServicesInteractions/HTTPService/OtherRequests.story");
-        configuredEmbedder().embedderControls().doGenerateViewAfterStories(true).doIgnoreFailureInStories(true)
-                .doIgnoreFailureInView(true).useStoryTimeoutInSecs(800);
-        configuration().useParameterControls(new ParameterControls().useDelimiterNamedParameters(true));
-    }
-}
+//public class DebugTestRunner extends SerenityStories {
+//
+//    public DebugTestRunner() {
+////        findStoriesCalled("stories/WebServicesInteractions/ResourceService/*");
+////        findStoriesCalled("stories/WebServicesInteractions/HTTPService/PostRequests.story;" +
+////                "stories/WebServicesInteractions/HTTPService/OtherRequests.story");
+//        configuredEmbedder().embedderControls().doGenerateViewAfterStories(true).doIgnoreFailureInStories(true)
+//                .doIgnoreFailureInView(true).useStoryTimeoutInSecs(800);
+//        configuration().useParameterControls(new ParameterControls().useDelimiterNamedParameters(true));
+//    }
+//}
